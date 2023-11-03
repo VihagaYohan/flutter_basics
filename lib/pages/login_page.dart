0926426e0a34b2084f370273d6fd8afaddcaed8e
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2023-11-03 08:26:10 +0530

    UI skelton for login page

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return const Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        Text("Hello, welcome back!"),
+        Text("Login to continue"),
+      ]
+    );
+  }
+
+}
---
+++ lib/widgets/scaffold.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class ScaffoldWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(),
+        floatingActionButton: FloatingActionButton(onPressed: () {}),
+        backgroundColor: Colors.greenAccent,
+        drawer: Container(width: 400, height: 400, color: Colors.blue[900]),
+        body: Column(children: <Widget>[
+          const Text("Hello, welcome back!"),
+          const Text("Login to continue"),
+          const TextField(
+            decoration: InputDecoration(hintText: 'user name'),
+          ),
+          const TextField(
+            decoration: InputDecoration(hintText: 'password'),
+          ),
+          TextButton(
+              onPressed: () {
+                if (kDebugMode) {
+                  print('forgot button clicked');
+                }
+              },
+              child: const Text('Forgot password ?')),
+          ElevatedButton(
+              onPressed: () {
+                print('login button clicked');
+              },
+              child: const Text("Login")),
+          const Text("Or sign in with"),
+          ElevatedButton(
+              onPressed: () {
+                if (kDebugMode) {
+                  print('google button clicked');
+                }
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset('assets/images/main_avatar.png',
+                      width: 25, height: 25),
+                  const Text('Login with google')
+                ],
+              )),
+          ElevatedButton(
+              onPressed: () {
+                if (kDebugMode) {
+                  print("facebook button clicked");
+                }
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset("assets/images/main_avatar.png",
+                      width: 25, height: 25),
+                  const Text("Login with facebook")
+                ],
+              )), 
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                const Text("Don't have account ?"),
+                TextButton(onPressed: () {
+                  if(kDebugMode) {
+                    print("sign up button clicked");
+                  }
+                }, child: const Text("Sign-up button"))
+              ],)
+        ]));
+  }
+}
