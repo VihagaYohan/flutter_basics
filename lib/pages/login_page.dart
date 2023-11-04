96fb8a4fb3c19a7d248c19bf65bf8ff73c5628f4
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2023-11-04 07:47:59 +0530

    Added basic styles

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -5,33 +5,67 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return  Scaffold(
+    return Scaffold(
         appBar: AppBar(),
         floatingActionButton: FloatingActionButton(onPressed: () {}),
         backgroundColor: Colors.greenAccent,
         drawer: Container(width: 400, height: 400, color: Colors.blue[900]),
         body: Column(children: <Widget>[
-          const Text("Hello, welcome back!"),
-          const Text("Login to continue"),
-          const TextField(
-            decoration: InputDecoration(hintText: 'user name'),
+          const Text(
+            "Hello, welcome back!",
+            style: TextStyle(
+                color: Colors.white, fontSize: 22, fontWeight: FontWeight.bold),
           ),
+          const SizedBox(height: 16),
+          const Text("Login to continue",
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+          const SizedBox(height: 12),
           const TextField(
-            decoration: InputDecoration(hintText: 'password'),
+            decoration: InputDecoration(
+                hintText: 'user name',
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(12))),
+                filled: true,
+                fillColor: Colors.white),
           ),
-          TextButton(
-              onPressed: () {
-                if (kDebugMode) {
-                  print('forgot button clicked');
-                }
-              },
-              child: const Text('Forgot password ?')),
-          ElevatedButton(
-              onPressed: () {
-                print('login button clicked');
-              },
-              child: const Text("Login")),
+          const SizedBox(height: 12),
+          const TextField(
+            decoration: InputDecoration(
+                hintText: 'password',
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(12))),
+                filled: true,
+                fillColor: Colors.white),
+          ),
+          Align(
+            alignment: Alignment.centerRight,
+            child: TextButton(
+                onPressed: () {
+                  if (kDebugMode) {
+                    print('forgot button clicked');
+                  }
+                },
+                style: TextButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    textStyle: const TextStyle(fontWeight: FontWeight.bold)),
+                child: const Text('Forgot password ?')),
+          ),
+          SizedBox(
+            width: 250,
+            child: ElevatedButton(
+                onPressed: () {
+                  print('login button clicked');
+                },
+                style:ElevatedButton.styleFrom(
+                  backgroundColor: Colors.amber,
+                  foregroundColor: Colors.black
+                ),
+                child: const Text("Login")),
+          ),
+          const SizedBox(height: 12),
           const Text("Or sign in with"),
+          const SizedBox(height: 12),
           ElevatedButton(
               onPressed: () {
                 if (kDebugMode) {
@@ -61,18 +95,20 @@ class LoginPage extends StatelessWidget {
                       width: 25, height: 25),
                   const Text("Login with facebook")
                 ],
-              )), 
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                const Text("Don't have account ?"),
-                TextButton(onPressed: () {
-                  if(kDebugMode) {
-                    print("sign up button clicked");
-                  }
-                }, child: const Text("Sign-up button"))
-              ],)
+              )),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text("Don't have account ?"),
+              TextButton(
+                  onPressed: () {
+                    if (kDebugMode) {
+                      print("sign up button clicked");
+                    }
+                  },
+                  child: const Text("Sign-up button"))
+            ],
+          )
         ]));
   }
-
 }
