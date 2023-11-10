ed4813995815065722fc028d9b710964db4183ab
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2023-11-10 06:19:03 +0530

    Added - pages to bottom navigation

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_playground/pages/home_page.dart';
 
 class MainPage extends StatefulWidget {
   @override
@@ -12,14 +13,16 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Bottom navigation container')),
-      body: const Center(child: Text("Center text")),
+      // appBar: AppBar(title: const Text('Bottom navigation container')),
+      body: pages[currentIndex],
       bottomNavigationBar: BottomNavigationBar(
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
           BottomNavigationBarItem(
               icon: Icon(Icons.favorite), label: 'Favorites'),
-          BottomNavigationBarItem(icon: Icon(Icons.add), label: 'Add Post')
+          BottomNavigationBarItem(icon: Icon(Icons.add), label: 'Add Post'),
+          BottomNavigationBarItem(icon: Icon(Icons.message), label: 'Message'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile")
         ],
         currentIndex: currentIndex,
         onTap: (index) {
@@ -27,10 +30,19 @@ class _MainPageState extends State<MainPage> {
             currentIndex = index;
           });
         },
+        type: BottomNavigationBarType.fixed,
         showSelectedLabels: false,
         showUnselectedLabels: false,
         backgroundColor: Colors.amber,
       ),
     );
   }
+
+  final pages = [
+    HomePage(),
+    Center(child: Text("Favorites")),
+    Center(child: Text("Add post")),
+    Center(child: Text('Messages')),
+    Center(child: Text('Profile'))
+  ];
 }
